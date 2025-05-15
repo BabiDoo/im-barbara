1e495298d084c57224c792cb0b80adc74466c2c7
Barbara <barbara.laisdorneles@gmail.com>
2025-05-15 18:05:05 -0300

    add href web app

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -36,7 +36,7 @@ const Projects = () => {
         </li>
         <li>
           <a
-            href="https://github.com/seu-usuario/webapp"
+            href="https://github.com/BabiDoo/crud-node-mongodb"
             target="_blank"
             rel="noopener noreferrer"
             className="text-blue-500 underline"
