6125731c3e227a37366485f28d257a5bb4da4df8
Barbara <barbara.laisdorneles@gmail.com>
2025-05-15 17:26:36 -0300

    updates

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -3,14 +3,66 @@ const Skills = () => {
     <main className="p-6">
       <h1 className="text-2xl font-bold mb-4">Skills & Certificates</h1>
       <ul className="list-disc list-inside space-y-2">
-        <li>Backend Certification (Meta)</li>
-        <li>Cybersecurity (Google)</li>
-        <li>Learning How to Learn (Barbara Oakley)</li>
+        <li>
+          <a
+            href="https://coursera.org/share/d56708d7908d654540fc0bacbb47f682"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 underline"
+          >
+            Backend Certification (Meta)
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://coursera.org/share/2dc16773f383df46c0d75f00aad1d2d6"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 underline"
+          >
+            Cybersecurity (Google)
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://coursera.org/share/e88b4175b01e689bfb5ed5477dcea944"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 underline"
+          >
+            API Basics
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://coursera.org/share/1b34c1f5fddd2ee32b8644b55b837386"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 underline"
+          >
+            Java Programming Fundamentals and OOP
+          </a>
+        </li>
+        <li>Web-App</li>
+
+        <li>Azure Fundamentals</li>
+
         <li>Dialogflow, Claude, MCP</li>
-        <li>Java / Python / C# / SQL / Git / Azure</li>
+        <li>Java / Python / Javascipt / C# / SQL / Git / Github</li>
+        <li>
+          <a
+            href="https://certs.duolingo.com/1c2fb030d0195c079ed8efb5544e0afa"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 underline"
+          >
+            Duolingo English Test
+          </a>
+        </li>
+        <li>"Learing How to Learn (Barbara Oakley)"</li>
       </ul>
     </main>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
